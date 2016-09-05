dcc0be04381a943afea55f23708e934f77f79cc2
Administrator <Administrator@172.16.13.69>
2016-09-05 09:56:37 +0800

    llz

---
+++ src/train/user/dao/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package train.user.dao.dao;
+
+import train.user.dao.vo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(String account);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(String account);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
