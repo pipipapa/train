6e4253b529c9d54c3cc78f15af4742e4be96f66f
Administrator <Administrator@172.16.13.87>
2016-09-13 09:18:25 +0800

    售票员cotroll

--- src/train/conductor/controll/ConductorCotroll.java
+++ src/train/conductor/controll/ConductorCotroll.java
@@ -1,5 +1,36 @@
 package train.conductor.controll;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import train.conductor.dao.vo.Conductor;
+import train.conductor.service.ConductorService;
+
+@Controller
 public class ConductorCotroll {
 
+	@Autowired
+	private ConductorService cservice;
+	
+	@RequestMapping("conductor/ConductorLogin")
+	public String login(String name,String pass,HttpServletRequest request)
+	{
+		System.out.println(name);
+		Conductor c=cservice.selectByName(name);
+		System.out.println(c);
+		if(c==null)
+		{
+			System.out.println("«Î÷ÿ–¬ ‰»Î’À∫≈£°");
+			return "redirect:login1.html";
+		}
+		if(c!=null&&c.getPass().equals(pass))
+		{
+			request.getSession().setAttribute("conductor", c);
+			return "redirect:index.html";
+		}
+		return null;
+	}
 }
